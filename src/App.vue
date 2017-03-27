dd63f874dfc539018d56035c771d60063357c136
yy9306 <1273460656@qq.com>
2017-03-27 17:50:12 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  {{c}}
+  <router-link :to = "{name: 'routerPage'}">Apple</router-link>
+  <router-link :to = "{path:'/banana'}">Banana</router-link>
+
+  <keep-alive>
+    <router-view></router-view>
+  </keep-alive>
+  </div>
+
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  data() {
+    return {
+      c: "c++"
+    }
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
